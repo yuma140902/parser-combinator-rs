4872b43a35e756c4406f4d22fc4b67cbbc6c96fd
yuma140902 <yuma140902@gmail.com>
2022-01-27 21:26:23 +0900

    Feat: 指定した一文字だけパースするパーサ`character`を追加

--- src/parsers/parsers.rs
+++ src/parsers/parsers.rs
@@ -14,9 +14,26 @@ pub fn digit<'a>(s: &'a str) -> Result<ParsingResult<'a, i32>, DenialReason> {
     }
 }
 
+pub fn character<'a>(c: char) -> impl Fn(&'a str) -> Result<ParsingResult<'a, ()>, DenialReason> {
+    move |s| {
+        let mut chars = s.chars();
+        if chars.next() == Some(c) {
+            Ok(ParsingResult {
+                first: (),
+                rest: chars.as_str(),
+            })
+        } else {
+            Err(DenialReason::Deny)
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::{common::ParsingResult, parsers::digit};
+    use crate::{
+        common::ParsingResult,
+        parsers::{character, digit},
+    };
 
     #[test]
     fn digit_true() {
@@ -66,5 +83,70 @@ mod tests {
         assert!(digit("        123").is_err());
         assert!(digit("漢字").is_err());
         assert!(digit("\u{1f363}").is_err());
+    }
+
+    #[test]
+    fn character_a() {
+        let parser = character('A');
+        assert_eq!(
+            parser("Abcd"),
+            Ok(ParsingResult {
+                first: (),
+                rest: "bcd"
+            })
+        );
+        assert_eq!(
+            parser("A"),
+            Ok(ParsingResult {
+                first: (),
+                rest: ""
+            })
+        );
+        assert!(parser("Banana").is_err());
+        assert!(parser("abcd").is_err());
+        assert!(parser("        A").is_err());
+        assert!(parser("").is_err());
+    }
+
+    #[test]
+    fn character_kanji() {
+        let parser = character('令');
+        assert_eq!(
+            parser("令和"),
+            Ok(ParsingResult {
+                first: (),
+                rest: "和"
+            })
+        );
+        assert_eq!(
+            parser("令"),
+            Ok(ParsingResult {
+                first: (),
+                rest: ""
+            })
+        );
+        assert!(parser("平成").is_err());
+        assert!(parser("         令").is_err());
+        assert!(parser("").is_err());
+    }
+
+    #[test]
+    fn character_emoji() {
+        let parser = character('\u{1f363}');
+        assert_eq!(
+            parser("\u{1f363}寿司"),
+            Ok(ParsingResult {
+                first: (),
+                rest: "寿司"
+            })
+        );
+        assert_eq!(
+            parser("\u{1f363}"),
+            Ok(ParsingResult {
+                first: (),
+                rest: ""
+            })
+        );
+        assert!(parser("\u{1f364}エビフライ").is_err());
     }
 }
